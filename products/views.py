d53f14d3ef3b96c407083fbb4524f4078fcf5a3f
Volkov Mkhail <volk.mixail@gmail.com>
2022-02-18 16:51:52 +0400

    added User models

--- products/views.py
+++ products/views.py
@@ -1,9 +1,5 @@
-import os
-import json
 from products.models import ProductCategory, Product
 from django.shortcuts import render
-
-MODULE_DIR = os.path.dirname(__file__)
 
 
 def index(request):
@@ -13,18 +9,11 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-# def products(request):
-#     file_path = os.path.join(MODULE_DIR, 'fixtures/products.json')
-#     context = {
-#         'title': 'GeekShop - Каталог',
-#         'products': json.load(open(file_path, encoding='utf-8')),
-#     }
-#     return render(request, 'products/products.html', context)
-
 def products(request):
     context = {
         'title': 'Geekshop - Каталог',
         'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
     }
 
     return render(request, 'products/products.html', context)
