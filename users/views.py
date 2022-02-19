d182f62b07d841d0050e287d0c9c6ca89c1a328c
Volkov Mkhail <volk.mixail@gmail.com>
2022-02-19 17:10:21 +0400

    added autorization

--- users/views.py
+++ users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth
+from users.forms import UserLoginForm
 
-# Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    form = UserLoginForm()
+    context = {'title': 'Geekshop - Авторизация', 'form': form}
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    context = {'title': 'Geekshop - Регистрация'}
+    return render(request, 'users/registration.html', context)
+
+
+
