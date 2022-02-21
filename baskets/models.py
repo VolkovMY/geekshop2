3ec7522dddfbd6642c5ca5cba8bdf1b9285a368b
Volkov Mkhail <volk.mixail@gmail.com>
2022-02-21 17:23:11 +0400

    added total_sum and total_quantity

--- baskets/models.py
+++ baskets/models.py
@@ -13,4 +13,13 @@ class Basket(models.Model):
     def __str__(self):
         return f'Корзина для {self.user.username} | Продукт {self.product.name}'
 
+    def sum(self):
+        return self.product.price * self.quantity
 
+    def total_quantity(self):
+        baskets = Basket.objects.filter(user=self.user)
+        return sum(basket.quantity for basket in baskets)
+
+    def total_sum(self):
+        baskets = Basket.objects.filter(user=self.user)
+        return sum(basket.sum() for basket in baskets)
