5aaab282460c00bd518106ee7fa97eb9fa214c00
mihanyga <volk.mixail@gmail.com>
2022-03-10 15:47:26 +0400

    modify django admin panel

--- admins/views.py
+++ admins/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import user_passes_test
 from users.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 
 
+@user_passes_test(lambda u: u.is_staff)
 def index(request):
     context = {'title': 'GeekShop - Admin'}
     return render(request, 'admins/index.html', context)
 
 
 # Read
+
+@user_passes_test(lambda u: u.is_staff)
 def admin_users(request):
     context = {'title': 'GeekShop - Пользователи', 'users': User.objects.all()}
     return render(request, 'admins/admin-users.html', context)
 
 
 # Create
+
+@user_passes_test(lambda u: u.is_staff)
 def admin_users_create(request):
     if request.method == 'POST':
         form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
@@ -29,6 +35,8 @@ def admin_users_create(request):
 
 
 # Update
+
+@user_passes_test(lambda u: u.is_staff)
 def admin_users_update(request, id):
     selected_user = User.objects.get(id=id)
     if request.method == 'POST':
@@ -43,6 +51,8 @@ def admin_users_update(request, id):
 
 
 # Delete
+
+@user_passes_test(lambda u: u.is_staff)
 def admin_users_delete(request, id):
     user = User.objects.get(id=id)
     user.safe_delete()
