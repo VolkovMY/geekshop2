3c216103faeafd9c382b9f8f988b25baa21deca9
mihanyga <volk.mixail@gmail.com>
2022-03-10 12:55:29 +0400

    added user-update form & controller

--- admins/views.py
+++ admins/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from users.models import User
-from admins.forms import UserAdminRegistrationForm
+from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
+
 
 def index(request):
     context = {'title': 'GeekShop - Admin'}
@@ -25,3 +26,17 @@ def admin_users_create(request):
         form = UserAdminRegistrationForm()
     context = {'title': 'GeekShop - Создание пользователя', 'form': form}
     return render(request, 'admins/admin-users-create.html', context)
+
+
+# Update
+def admin_users_update(request, id):
+    selected_user = User.objects.get(id=id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(instance=selected_user, files=request.FILES, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=selected_user)
+    context = {'title': 'GeekShop - Редактирование пользователя', 'selected_user': selected_user, 'form': form}
+    return render(request, 'admins/admin-users-update-delete.html', context)
