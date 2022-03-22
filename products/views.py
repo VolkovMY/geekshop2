3e26a4940f799f2043c6bda4373589219204f666
mihanyga <volk.mixail@gmail.com>
2022-03-22 09:59:53 +0400

    added filtering by category_id

--- products/views.py
+++ products/views.py
@@ -9,11 +9,8 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request):
-    context = {
-        'title': 'Geekshop - Каталог',
-        'products': Product.objects.all(),
-        'categories': ProductCategory.objects.all(),
-    }
-
+def products(request, category_id=None):
+    context = {'title': 'Geekshop - Каталог', 'categories': ProductCategory.objects.all(), }
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+    context['products'] = products
     return render(request, 'products/products.html', context)
