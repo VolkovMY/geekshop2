f92aa3f4be64759b6bd52a50af2b3c6fe54deeb4
Volkov Mkhail <volk.mixail@gmail.com>
2022-06-07 19:03:22 +0400

    added VK auth, added model UserProfile

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from products.views import index, products
+from products.views import index
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('social/', include('social_django.urls', namespace='social')),
     path('products/', include('products.urls', namespace='products')),
     path('users/', include('users.urls', namespace='users')),
     path('baskets/', include('baskets.urls', namespace='baskets')),
--- users/models.py
+++ users/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from datetime import timedelta
 from django.utils.timezone import now
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 def get_activation_key_expires():
@@ -26,3 +28,25 @@ class User(AbstractUser):
     def safe_delete(self):
         self.is_active = False
         self.save()
+
+
+class UserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+    NON_BINARY = 'N'
+
+    GENDER_CHOICES = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (NON_BINARY, 'Non binary'),
+    ]
+
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
+    gender = models.CharField(User, max_length=1, choices=GENDER_CHOICES)
+    about = models.TextField(User, blank=True)
+
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            profile = UserProfile(user=instance)
+            profile.save()
